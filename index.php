50a38054359402600842d875b3e06bb85df3c0e6
DiegoAneli <diegoaneli@gmail.com>
2019-02-13 17:12:13 +0100

    Post articolo

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php get_header(); ?>
 
-  <h1>Ciao</h1>
+  <div class="container">
+
+    <div class="row">
+      <div class="col-8">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+          <div class="post">
+            <h2>
+                <a href="<?php the_permalink(); ?>"></a>
+                  <?php the_title(); ?>
+                  <small><?php the_time('j F Y'); ?></small>
+
+            </h2>
+            <p><?php the_excerpt(); ?></p>
+            <h3>Scritto da: <?php the_author_posts_link(); ?></h3>
+
+
+          </div>
+
+        <?php endwhile; else: ?>
+          <p>Nessun articolo disponibile</p>
+        <?php endif; ?>
+
+
+      </div>
+      <div class="col-4">
+        <?php get_sidebar(); ?>
+
+      </div>
+    </div>
+
+  </div>
 
 <?php get_footer(); ?>
